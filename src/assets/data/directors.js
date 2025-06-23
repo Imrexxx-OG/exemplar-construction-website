26e5683344f1c5af4e3d7a4bebb002927efca853
Imrexxx-OG <imrexxx93@gmail.com>
2025-06-23 19:45:33 +0100

    second commit

--- src/assets/data/directors.js
+++ src/assets/data/directors.js
@@ -1,18 +1,59 @@
+import executiveDirector from '../images/⁠executive_director.jpeg';
+import permanentSecretary from '../images/permanent_secretary.jpeg';
+import directortech from '../images/director_tech.jpeg';
+import directorhrm from '../images/directorHRM.jpeg';
+import profilePic1 from '../images/profile-silhouette-png-image.png';
+import profilePic2 from '../images/profile-silhouette-png-image.png';
+import profilePic3 from '../images/profile-silhouette-png-image.png';
+
 export const directors = [
   {
     id: 1,
-    name: "John Doe",
-    title: "CEO",
+    name: "Abdullahi Muhammad Kabir",
+    title: "Executive Director (C.E.O)",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image: "/images/ceo.jpg",
+    image: executiveDirector
   },
-  // Add 7 more directors with similar structure
   {
     id: 2,
-    name: "Jane Smith",
-    title: "Director of Operations",
+    name: "Ahmad Umar Aliyu",
+    title: "Permanent Secretary",
     bio: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    image: "/images/director1.jpg",
+    image: permanentSecretary
   },
-  // ... continue for others
+  {
+    id: 3,
+    name: "Hussaini Muhammad",
+    title: "Director of Finance",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: profilePic1
+  },
+  {
+    id: 4,
+    name: "Salihu Nasiru Muhammad",
+    title: "Director of Human Resources",
+    bio: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: directorhrm
+  },
+  {
+    id: 5,
+    name: "Garu Boy, MD",
+    title: "Director of Operations",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: profilePic2
+  },
+  {
+    id: 6,
+    name: "Lawal Wappah",
+    title: "Director of Marketing",
+    bio: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: profilePic3
+  },
+  {
+    id: 7,
+    name: "Babangida Abba Imran",
+    title: "Director of Technology",
+    bio: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    image: directortech
+  }
 ];
